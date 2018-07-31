656a587deba3a8a50666c9d2da17c2f9a575798a
huangzugang <1981929112@qq.com>
2018-07-31 15:30:57 +0800

    1. CI Test

---
+++ CITest2/CITest2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CITest2
+//
+//  Created by hzg on 2018/7/31.
+//  Copyright © 2018年 tz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
